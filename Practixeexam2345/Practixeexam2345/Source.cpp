4409642bdffcd29874fdb7e8ddc23f245af04fbc
Anthony <70087074+Anthonygg7@users.noreply.github.com>
2021-02-10 19:57:29 -0800

    Add files via upload

---
+++ Practixeexam2345/Practixeexam2345/Source.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+int main()
+
+{
+	//variables
+	//Get input
+	//validate input
+	do
+	{ //cout 
+		//cin
+
+	} while (variable is not a positive value);
+
+	//Set up condtions 
+	if (...);
+	{
+		//calculate the price 
+		//calculate the discount
+	}
+	else if (...);
+		return 0;
+}
